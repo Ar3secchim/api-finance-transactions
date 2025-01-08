0652cc21b919027ba0385cd7e3d37a58442e45f1
Renara Secchimm <renarasecchim@gmail.com>
2025-01-08 11:26:23 -0300

    fix: :bug: Add @Autowired annotation to ListCustomer for dependency injection

--- src/main/java/com/crud/modules/customers/usecase/ListCustomer.java
+++ src/main/java/com/crud/modules/customers/usecase/ListCustomer.java
@@ -2,6 +2,7 @@ package com.crud.modules.customers.usecase;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.crud.modules.customers.DAO.CustomerDAO;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ListCustomer {
+  @Autowired
   private CustomerDAO customerDAO;
 
   public List<CustomerResponse> execute() {
