df346be88b036c8a8b93846aa9f28c09c20d0d8b
Renara Secchimm <renarasecchim@gmail.com>
2025-01-09 11:09:11 -0300

    refactor: :recycle: Update execute method in RegisterCustomer to throw specific exceptions for validation errors

--- src/main/java/com/crud/modules/customers/usecase/RegisterCustomer.java
+++ src/main/java/com/crud/modules/customers/usecase/RegisterCustomer.java
@@ -3,7 +3,8 @@ package com.crud.modules.customers.usecase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.crud.infra.exception.BadRequestClient;
+import com.crud.infra.exception.DuplicateCpfException;
+import com.crud.infra.exception.InvalidRequestException;
 import com.crud.modules.customers.DAO.CustomerDAO;
 import com.crud.modules.customers.DTO.CustomerResponse;
 import com.crud.modules.customers.entity.Customer;
@@ -20,24 +21,24 @@ public class RegisterCustomer {
   private CustomerDAO customerDAO;
 
   @Transactional
-  public CustomerResponse execute(Customer customer) throws Exception {
+  public CustomerResponse execute(Customer customer) throws InvalidRequestException, DuplicateCpfException {
     // TODO - Implementar validação de CPF
     customer.setCpf(customer.getCpf().replaceAll("[^0-9]", ""));
 
     if (!ValidateCpf.execute(customer.getCpf())) {
-      throw new BadRequestClient("Invalid CPF");
+      throw new InvalidRequestException("Invalid CPF");
     }
 
     if (customerDAO.findByCpf(customer.getCpf()) != null) {
-      throw new BadRequestClient("CPF já está cadastrado");
+      throw new DuplicateCpfException("CPF já está cadastrado");
     }
 
     if (customer.getName() == null || customer.getName().isEmpty()) {
-      throw new BadRequestClient("Name is required");
+      throw new InvalidRequestException("Name is required");
     }
 
     if (customer.getAge() == null || customer.getAge() < 18) {
-      throw new BadRequestClient("Age is required and must be greater than 18");
+      throw new InvalidRequestException("Age is required and must be greater than 18");
     }
 
     Customer customerSave = customerDAO.SaveAndFetch(customer);
